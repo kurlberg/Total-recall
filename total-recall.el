f82f9ccba59977bd6f6d51904a905280f085de22
Par Kurlberg <kurlberg@gmail.com>
2011-02-22 23:33:27 +0100

    Got rid of pk-prefixes in the code.

--- total-recall.el
+++ total-recall.el
@@ -21,12 +21,12 @@
 ;;;; Things for the user to configure.  However, we try to set things
 ;;;; up to be "zero config", with reasonable defaults etc.
 
-(defvar pk-total-recall-directory 
+(defvar total-recall-directory 
   (concat (getenv "HOME") "/.total-recall")
   "Directory where web pages will be kept.")
 
-(defvar pk-checksum-programs '("shasum" "gsha1sum" "sha1sum" 
-			       "md5sum" "cksum")
+(defvar total-recall-checksum-programs 
+  '("shasum" "gsha1sum" "sha1sum" "md5sum" "cksum")
   "List of possible checksum programs, in order of preference.  
 Feel free to add more!")
 
@@ -34,63 +34,63 @@ Feel free to add more!")
 
 ;; Zeroconf way of selecting an executable.
 
-(defun pk-util-select-executable (l)
+(defun total-recall-select-executable (l)
   (if (null l)
       (error "No binary in list available!")
     (let ((executable (executable-find (car l))))
       (if executable
 	  executable
-	(pk-util-select-executable (cdr l))))))
+	(total-recall-select-executable (cdr l))))))
 
-(defvar pk-checksum-program
+(defvar total-recall-checksum-program
   ;; MAYBE FIXME: Maybe better if the binary is fixed after a first
   ;; inital selection - what if the user shares/syncs his
   ;; ~/.total-recall between several computers, and they have
   ;; different checksum programs installed!?  (Not a massive problem,
   ;; the user can later run a program which renames files according to
   ;; their (new) checksum.
-  (pk-util-select-executable pk-checksum-programs)
+  (total-recall-select-executable total-recall-checksum-programs)
   "Which binary to use for checksums.")
 
 ;; Create directory for saving history if it does not already exist.
-(unless (file-exists-p pk-total-recall-directory)
-  (make-directory pk-total-recall-directory))
+(unless (file-exists-p total-recall-directory)
+  (make-directory total-recall-directory))
 
 ;; FIXME: bad style to use defvar here?
-(defvar pk-tmp-file-name (concat pk-total-recall-directory "/tc-test"))
+(defvar total-recall-tmp-file-name (concat total-recall-directory "/tc-test"))
 
-(defun pk-get-cksum ()
+(defun total-recall-get-cksum ()
   "Compute cksum of file corresponding to temporarily saved buffer."
   ; Note: use as strong checksum algorithm as possible; say SHA or
   ; md5.  Likelyhood of adversary making html pages with intentional
   ; collisions is probably extremely small, but...
   (car (split-string
 	(shell-command-to-string 
-	 (concat pk-checksum-program " " pk-tmp-file-name)))))
+	 (concat total-recall-checksum-program " " total-recall-tmp-file-name)))))
 ;;JAVE TODO do a process here... async and works with tramp.
 ;; FIXME: hack away! :-)
 ;; Actually, is this worth doing?  The text is saved *after* async
 ;; downloading, and the actual saving and computing of the checksum
 ;; ought to be "instant".
 
-(defun pk-save-page (url)
+(defun total-recall-save-page (url)
   ; Dump web page text to file, compute its checksum, and rename file
   ; to checksum.  
 
   ; Note: functions in w3m-display-hook are called with the url in
   ; question as an argument, but we don't use it.
   (let ((coding-system-for-write 'utf-8))
-    (write-region nil nil pk-tmp-file-name t)
-    ;(message (pk-get-cksum))
-    (rename-file pk-tmp-file-name 
-		 (concat pk-total-recall-directory "/" (pk-get-cksum))
+    (write-region nil nil total-recall-tmp-file-name t)
+    (rename-file total-recall-tmp-file-name 
+		 (concat total-recall-directory "/" 
+			 (total-recall-get-cksum))
 		 t)))
 
-(add-hook 'w3m-display-hook 'pk-save-page)
+(add-hook 'w3m-display-hook 'total-recall-save-page)
 
 ;; Remark: Advising w3m-goto-url does not work; pages are retrived
 ;; asyncronously, and the adviced code is sometimes run *before* the
 ;; text has been fetched.  Switching to a hook instead: stuff in
 ;; w3m-display-hook is run *after* the web page has been fetched.
 
-(provide 'pk-total-recall)
+(provide 'total-recall)
